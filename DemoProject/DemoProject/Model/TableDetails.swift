6015c8a7650a4e3ad58b78e638919d88c8e43ae1
Manisha Vashisth <manishavashisth@Manishas-MacBook-Air.local>
2023-05-03 22:35:04 +0530

    Add updating project files to Git

---
+++ DemoProject/DemoProject/Model/TableDetails.swift
@@ -0,0 +1,8 @@
+//
+//  TableDetails.swift
+//  DemoProject
+//
+//  Created by Manisha Vashisth on 01/05/23.
+//
+
+import Foundation
---
+++ DemoProject/DemoProject/ViewModel/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  DemoProject
+//
+//  Created by Manisha Vashisth on 01/05/23.
+//
+
+import Foundation
